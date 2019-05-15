d280615d5b231cd8fce350fe62b4d4db6f09efdb
LQH <15270854417@163.com>
2019-05-15 20:02:39 +0800

    chapter01 over

--- chapter01/hello_world.py
+++ chapter01/hello_world.py
@@ -8,4 +8,13 @@ print(message1.title())
 print(message1.upper())
 print(message1.lower())
 
+3**2
+3*2
 
+favorite_language="' test test'"
+print(favorite_language.rstrip())
+print(favorite_language)
+
+age=23
+birthday='happy'+str(age)+"rd birthday"
+print(birthday)
